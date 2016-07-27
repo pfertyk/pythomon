5f11f905c69930e13ca0d85447aa8040ce42415d
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-07-27 20:13:15 +0200

    Move player

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ class Player:
     def __init__(self):
         self.coords = [MAP_WIDTH//2, MAP_HEIGHT//2]
 
+    def move(self, vector):
+        self.coords[0] += vector[0]
+        self.coords[1] += vector[1]
+
 if __name__ == '__main__':
     player = Player()
     print(player.coords)
 
     while True:
         command = input()
-        print(command)
 
-        if command == 'exit':
+        if command == 'exit' or command == 'quit' or command == 'q':
             break
+        elif command == 'right':
+            player.move([1, 0])
+        elif command == 'left':
+            player.move([-1, 0])
+        elif command == 'up':
+            player.move([0, 1])
+        elif command == 'down':
+            player.move([0, -1])
+        else:
+            print('Command was not recognized')
+
+        print(player.coords)
