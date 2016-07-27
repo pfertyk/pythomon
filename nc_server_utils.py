d2edec3ac948784f4708e8453e1e98d3b7d8ed74
Przemysław Adamek <adw1n@users.noreply.github.com>
2016-07-27 20:35:59 +0200

    Not one conflict, today was a good day.

--- nc_server_utils.py
+++ nc_server_utils.py
@@ -1,47 +1,37 @@
 import socket
-import sys
 HOST="0.0.0.0"
-HOST="localhost"
 PORT=10000
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (HOST, PORT)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(server_address)
-sock.listen(10)
+DEBUG=False
+connection=None
 def recv_until_newline(connection : socket.socket)->str:
     msg=""
     while True:
         msg+=connection.recv(1).decode("utf-8")
         if "\n" in msg:
             return msg
+
 def init_sock():
-    pass
-
-
-def mysend(sock, msg):
-    totalsent = 0
-    while totalsent < len(msg):
-        sent = sock.send(msg[totalsent:])
-        if sent == 0:
-            raise RuntimeError("socket connection broken")
-        totalsent = totalsent + sent
-while True:
+    if DEBUG:
+        return
+    global connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (HOST, PORT)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(server_address)
+    sock.listen(10)
     connection, client_address = sock.accept()
-    flo = connection.makefile()
-    try:
-        print('client connected: %s'%(client_address,))
-        while True:
-            # flo.write("sup")
-            # flo.flush()
-            msg=recv_until_newline(connection)
-            # mysend(sock,str.encode("recved %s"%msg))
-            connection.sendall(str.encode("recved %s"%msg))
-            # if data:
-            #     connection.sendall(data)
-            # else:
-            #     break
-    except Exception as e:
-        print(e)
-        connection.close()
-    finally:
-        connection.close()
+def read():
+    if DEBUG:
+        return input()
+    else:
+        return recv_until_newline(connection)
+def send(msg : str):
+    if DEBUG:
+        print(msg)
+    else:
+        connection.sendall(str.encode(msg))
+while True:
+    init_sock()
+    while True:
+        msg=read()
+        send(msg)
