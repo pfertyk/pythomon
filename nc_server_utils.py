fb93be269899f6e4f781237cb97175f097554701
Przemysław Adamek <adw1n@users.noreply.github.com>
2016-07-27 20:52:49 +0200

    this is how we generate our shit.

--- nc_server_utils.py
+++ nc_server_utils.py
@@ -1,38 +1,42 @@
 import socket
-HOST="0.0.0.0"
-PORT=10000
-# DEBUG=True
-connection=None
-def recv_until_newline(connection : socket.socket)->str:
-    msg=""
-    while True:
-        msg+=connection.recv(1).decode("utf-8")
-        if "\n" in msg:
-            return msg.replace("\n","")
 
-def init_sock(DEBUG : bool):
-    if DEBUG:
-        return
-    global connection
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (HOST, PORT)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(server_address)
-    sock.listen(10)
-    connection, client_address = sock.accept()
-def read(DEBUG : bool):
-    if DEBUG:
-        return input()
-    else:
-        return recv_until_newline(connection)
-def send(msg : str, DEBUG: bool):
-    if DEBUG:
-        print(msg)
-    else:
-        connection.sendall(str.encode(msg))
-if __name__ == "__main__":
-    while True:
-        init_sock()
+# DEBUG=True
+class Communicator():
+    HOST = "0.0.0.0"
+    PORT = 10000
+    connection=None #type: socket.socket
+    def __init__(self,DEBUG=True):
+        self.DEBUG=DEBUG
+        self.init_sock()
+    def recv_until_newline(self)->str:
+        msg=""
         while True:
-            msg=read()
-            send(msg)
+            msg+=self.connection.recv(1).decode("utf-8")
+            if "\n" in msg:
+                return msg.replace("\n","")
+
+    def init_sock(self):
+        if self.DEBUG:
+            return
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.HOST, self.PORT)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(server_address)
+        sock.listen(10)
+        self.connection, client_address = sock.accept()
+    def read(self):
+        if self.DEBUG:
+            return input()
+        else:
+            return self.recv_until_newline()
+    def send(self,msg : str):
+        if self.DEBUG:
+            print(msg)
+        else:
+            self.connection.sendall(str.encode(msg))
+# if __name__ == "__main__":
+#     while True:
+#         init_sock()
+#         while True:
+#             msg=read()
+#             send(msg)
