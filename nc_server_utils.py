ab67650f5ebbd2b6a2be4499288fba30d957f96a
Paweł Fertyk <pfertyk@openmailbox.org>
2016-09-10 00:49:30 +0200

    Fix PEP8 errors

--- nc_server_utils.py
+++ nc_server_utils.py
@@ -1,19 +1,22 @@
 import socket
 
+
 # DEBUG=True
 class Communicator():
     HOST = "0.0.0.0"
     PORT = 10000
-    connection=None #type: socket.socket
-    def __init__(self,DEBUG=True):
-        self.DEBUG=DEBUG
+    connection = None  # type: socket.socket
+
+    def __init__(self, DEBUG=True):
+        self.DEBUG = DEBUG
         self.init_sock()
+
     def recv_until_newline(self)->str:
-        msg=""
+        msg = ""
         while True:
-            msg+=self.connection.recv(1).decode("utf-8")
+            msg += self.connection.recv(1).decode("utf-8")
             if "\n" in msg:
-                return msg.replace("\n","")
+                return msg.replace("\n", "")
 
     def init_sock(self):
         if self.DEBUG:
@@ -24,14 +27,16 @@ class Communicator():
         sock.bind(server_address)
         sock.listen(10)
         self.connection, client_address = sock.accept()
-    def input(self,msg=""):
+
+    def input(self, msg=""):
         if self.DEBUG:
             return input(msg)
         else:
             if msg:
                 self.print(msg)
             return self.recv_until_newline()
-    def print(self,msg : str):
+
+    def print(self, msg: str):
         if self.DEBUG:
             print(msg)
         else:
